531255aa02a5b54ae5ac2f34b169711021c8d05f
julescosta <jcosta0295@gmail.com>
2021-09-27 18:50:09 -0400

    added seller class

---
+++ seller.py
@@ -0,0 +1,14 @@
+class seller():
+    """Initializes the seller class.
+
+
+    This class defines the attributes of a seller including the seller's inventory, customer reviews, history,
+    average rating, and total number of ratings.
+    """
+
+    def __init__(self, inventory, reviews, history, rating, numRating):
+        self.inventory = {}
+        self.reviews = {}
+        self.history = []
+        self.rating = 0
+        self.numRating = 0
