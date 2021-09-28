23db9de7cbb1bdcdc62aedd4aff119c0433f2d0d
aferafer <17afer@queensu.ca>
2021-09-27 20:22:29 -0400

    fixed line length

--- buyerAndUser.py
+++ buyerAndUser.py
@@ -1,16 +1,17 @@
 #This class stores the information related to the user's buyer account
-class buyer():
+class Buyer():
 
-    def __init__(self, purchaseHistory, activeAuctions, savedSellers, recentlyViewed, watchList):
-        purchaseHistory = [] #past purchases made from the user's buyer account
+    def __init__(self, purchaseHistory, activeAuctions, savedSellers,
+                 recentlyViewed, watchList):
+        purchaseHistory = [] #past purchases made
         activeAuctions = []  #current auctions the user is taking part in
         savedSellers = [] #'favorites' list of sellers for the user
         recentlyViewed = [] #stores the top 10 most recently viewed items
-        watchList = [] #users can add items to their watch list to automatically recieve updates on them
+        watchList = [] #users can add items to their watch list
 
 
 #This class is responsible for storing the users account information    
-class user():
+class User():
 
     def __init__(self, username, password, accountBalance):
     	username = ""
