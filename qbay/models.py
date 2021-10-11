81a430415945e1d33dd101b0d11eed53fed86426
SophiaPerzan <sophia@perzan.dev>
2021-10-10 23:33:31 -0400

    Tests added

--- qbay/models.py
+++ qbay/models.py
@@ -1,12 +1,11 @@
+import string
 from qbay import app
 from flask_sqlalchemy import SQLAlchemy
-
+from datetime import date
 
 '''
 This file defines data models and related business logics
 '''
-
-
 db = SQLAlchemy(app)
 
 
@@ -19,6 +18,7 @@ class User(db.Model):
         primary_key=True)
     password = db.Column(
         db.String(120), nullable=False)
+    products = db.relationship('product', backref="user",lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -36,17 +36,17 @@ class transaction(db.Model):
 class product(db.Model):
     """Creates the product entity and related attributes in the database."""
     ID = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120), nullable=False)
-    description = db.Column(db.String(2000), nullable=False)
+    title = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=False)
     price = db.Column(db.Float, nullable=False)
-    lastModDate = db.Column(db.String(20), nullable=False)
-    ownerEmail = db.Column(
-        db.String, db.ForeignKey('user.email'),
-        nullable=False)
+    lastModDate = db.Column(db.Date, nullable=False)
+    #ownerEmail = db.Column(db.String, db.ForeignKey('user.user_email'),
+     #                      nullable=False)
 
 
 # create all tables
 db.create_all()
+
 
 
 def register(name, email, password):
@@ -92,53 +92,56 @@ def login(email, password):
 def create_product(product_title, product_description, price,
                    last_modified_date, owner_email):
     """Creates a product under a user.
-
     Check if all the attributes being created are valid before creating 
     the product in the database."""
     # Test if the title is correct
-    if ((not product_title.isalnum()) or product_title[0] == " " or
-            product_title[-1] == " "):
-        return "The title has to be alphanumeric only and the"\
-            "first and last character cannot be space."
+    if product_title[0] == " " or product_title[-1] == " " or (
+            not product_title.replace(" ", "").isalnum()):
+        print("The title has to be alphanumeric only and the "
+              "first and last character cannot be space.")
+        return None
 
     if len(product_title) > 80:
+        return None
         print("Length of the product title cannot exceed 80 characters")
 
     if (len(product_description) < 20 or len(product_description) > 2000):
-        return "Lenght of the product description should be at least "\
-            "20 characters and at most 2000 characters"
+        return None
+        print("Lenght of the product description should be at least "
+              "20 characters and at most 2000 characters")
 
     if len(product_description) <= len(product_title):
-        return "Length of description must be longer than the product's title."
+        return None
+        print("Length of description must be longer than the product's title.")
 
     if price < 10 or price > 10000:
-        return "Price has to be within the range of [10,10000]"
+        print("Price has to be within the range of [10,10000].")
+        return None
 
     # Check if the date is within range
-    dates = last_modified_date.split("-")
-    if dates[0] >= "2021" and dates[0] <= "2025":
-        if dates[0] == "2025":
-            if dates[1] > "01":
-                return "Date has to be after 2021-01-02 and before 2025-01-02."
-            elif dates[2] > "02":
-                return "Date has to be after 2021-01-02 and before 2025-01-02."
-    else:
-        return "Date has to be after 2021-01-02 and before 2025-01-02."
+    if last_modified_date < date(
+            2021, 1, 2) or last_modified_date > date(
+            2025, 1, 2):
+        print("Date has to be between 2021-01-02 and 2025-01-02.")
+        return None
 
     # Check if own email is empty
     if (len(owner_email) == 0):
-        return "Owner email cannot be empty"
+        print("Owner email cannot be empty.")
+        return None
 
     # Check if the owner exists in the database
     exist_owner = User.query.filter_by(email=owner_email).first()
     if exist_owner is None:
-        return "The user doesn't exist in the data base"
+        print("The user doesn't exist in the data base.")
+        return None
 
     # Check if the title already exists under the same user
     exist_title = product.query.filter_by(
         ownerEmail=owner_email, title=product_title)
     if exist_title is not None:
-        return "The product title already exits under the same user."
+        print("The product title already exits under the same user.")
+        return None
 
     # Add the product under the user database
     new_product = product(
@@ -150,4 +153,47 @@ def create_product(product_title, product_description, price,
     # actually save the product object
     db.session.commit()
 
+    return True
+
+def updateProduct(ID, newID, title, description, price, ownerEmail):
+    productToUpdate = product.query.filter_by(ID=ID, ownerEmail=ownerEmail).first()
+    if(verifyProductInputs(productToUpdate, newID, title, description, price) and
+       date.today() > date(2021, 1, 2) and date.today < date(2025, 1, 2)):
+        productToUpdate.ID = newID
+        productToUpdate.title = title
+        productToUpdate.description = description
+        productToUpdate.price = price
+        productToUpdate.lastModDate = date.today()
+        return True
+    else:
+        return False
+# Take in the parameters and check them for conformity with specifications
+def verifyProductInputs(product, newID, title, description, price):
+    oldPrice = product.price
+    oldID = product.ID
+# If title is not less than or equal to 80 chars, isnt alphanumeric +
+# spaces (excluding prefix and suffix whitespace) then return false
+    if (not(len(title) <= 80 and title.replace(' ', '').isalnum() and
+        (title[:1] == ' ' or title[:1].isalnum()) and
+        (title[len(title) - 1:len(title)] == ' ' or
+         title[len(title) - 1:len(title)].isalnum()))):
+        return False
+    #Check that the title isn't already in use
+    if (product.query.filter_by(title=title).all()>0):
+        return False
+    
+        # If the description is not greater than or equal to 20 chars, less
+        # than or equal to 2000 chars, and is not longer than title, return
+        # false
+    if (not(len(description) >= 20 and len(description)
+            <= 2000 and len(description) > len(title))):
+        return False
+        # Price must be greater and fall in the bounds of 10 to 10,000
+        # inclusive
+    if (not(price > oldPrice and price >= 10 and price <= 10000)):
+        return False
+        # The new ID must not already be in use
+    if (not(newID == oldID or
+            product.query.filter_by(ID=newID, ownerEmail = product.ownerEmail).first() is None)):
+        return False
     return True
--- qbay_test/test_models.py
+++ qbay_test/test_models.py
@@ -1,4 +1,5 @@
-from qbay.models import register, login, create_product
+from qbay.models import register, login, create_product, updateProduct, product,User
+from datetime import date
 
 
 def test_r1_7_user_register():
@@ -9,7 +10,73 @@ def test_r1_7_user_register():
     assert register('u0', 'test0@test.com', '123456') is True
     assert register('u0', 'test1@test.com', '123456') is True
     assert register('u1', 'test0@test.com', '123456') is False
+    print()
 
+def test_updateProduct():
+    #create a user to have the email for the product below
+    register("u002", "someone@exmaple.com","123456")
+    
+    #create a valid product to be used for some test cases, eg title collisions
+    create_product("Smartphone", "The best smartphone money can buy", 999.99,
+                   date(2022,4,15), "someone@example.com")
+    ID = 30
+    newID = 30
+    title = "Title"
+    description = "A description longer than title, for a great product"
+    price = 35.50
+    ownerEmail = "someone@example.com"
+    create_product(title, description, price, date(2022,4,15), "someone@example.com")
+    ID = product.query.filter_by(ownerEmail=ownerEmail, title=title).first().ID
+    #All params valid
+
+    #Title not valid - too long
+    title += "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    title+= "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
+    assert updateProduct(ID, newID, title, description, price, ownerEmail) is False
+    #Title not valid - not alphanum
+    title = "not alphanumeric!"
+    assert updateProduct(ID, newID, title, description, price, ownerEmail) is False
+    #Title not valid - space as prefix
+    title = " space as prefix"
+    assert updateProduct(ID, newID, title, description, price, ownerEmail) is False
+    #Title not valid - space as suffix
+    title="space as suffix "
+    assert updateProduct(ID, newID, title, description, price, ownerEmail) is False
+    #Title not valid, already exists
+    title = "Smartphone"
+    assert updateProduct(ID, newID, title, description, price, ownerEmail) is False
+    #description not valid - To short <20 chars
+    description = "<20chars"
+    title = "A Valid Title"
+    assert updateProduct(ID, newID, title, description, price, ownerEmail) is False
+    #description not valid - Shorter than title
+    title = "A shorter title"
+    description = "Short description here"
+    assert updateProduct(ID, newID, title, description, price, ownerEmail) is False
+    #description not valid - too long
+    for i in range(60):
+        description+="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    assert updateProduct(ID, newID, title, description, price, ownerEmail) is False
+    #Price not valid - too low
+    description = "A Valid description"
+    price = 5.0
+    assert updateProduct(ID, newID, title, description, price, ownerEmail) is False
+    #Price not valid - too high
+    price = 20000.0
+    assert updateProduct(ID, newID, title, description, price, ownerEmail) is False
+    #Price not valid - lower than current price
+    price = 25.0
+    assert updateProduct(ID, newID, title, description, price, ownerEmail) is False
+    price  = 40.0
+
+    #Modified date out of bounds, too early
+    # assert updateProduct(ID, newID, title, description, price) is False
+    #Modified date out of bounds, too late
+    #assert updateProduct(ID, newID, title, description, price) is False
+
+    #newID not valid, already exists
+    newID = product.query.filter_by(title="Smartphone").first().ID
+    assert updateProduct(ID, newID, title, description, price, ownerEmail) is False
 
 def test_r2_1_login():
     '''
@@ -30,40 +97,39 @@ def test_r2_1_login():
 def test_create_product():
     """
     Testing the create_product function.
-
-    In this function, different test cases will be created to test out all the 
+    In this function, different test cases will be created to test out all the
     constraints under title, description, price, date and email address.
     """
     # Create a list of correct variables
     correct_title = "smartwatch"
     correct_description = "A small smartphonelike device worn on wrist."
     correct_price = 50
-    correct_date = "2021-10-05"
+    correct_date = date(2020, 10, 5)
     correct_email = "abc@gmail.com"
     # Test incorrect alphanumeric title
     test_title_1 = "wrong, title"
-    print(create_product(test_title_1, correct_description,
-                         correct_price, correct_date, correct_email))
-    print("--------------------------------------------------------")
+    product_1 = create_product(test_title_1, correct_description,
+                               correct_price, correct_date, correct_email)
+    assert product_1 is None
     # Test incorrect title with space prefix
     test_title_2 = " wrongtitle"
-    print(create_product(test_title_2, correct_description,
-                         correct_price, correct_date, correct_email))
-    print("--------------------------------------------------------")
+    product_2 = create_product(test_title_2, correct_description,
+                               correct_price, correct_date, correct_email)
+    assert product_2 is None
     # Test incorrect title with space suffix
     test_title_3 = "wrongtitle "
-    print(create_product(test_title_3, correct_description,
-                         correct_price, correct_date, correct_email))
-    print("--------------------------------------------------------")
+    product_3 = create_product(test_title_3, correct_description,
+                               correct_price, correct_date, correct_email)
+    assert product_3 is None
     # Test incorrect title longer than 80 characters (81)
     test_title_4 = ""
     k = 0
     while k <= 80:
         test_title_4 += "a"
         k += 1
-    print(create_product(test_title_4, correct_description,
-                         correct_price, correct_date, correct_email))
-    print("--------------------------------------------------------")
+    product_4 = create_product(test_title_4, correct_description,
+                               correct_price, correct_date, correct_email)
+    assert product_4 is None
 
     # Test description less than 20 characters (19)
     test_des_1 = ""
@@ -71,71 +137,70 @@ def test_create_product():
     while i < 20:
         test_des_1 += "a"
         i += 1
-    print(create_product(correct_title, test_des_1,
-                         correct_price, correct_date, correct_email))
-    print("--------------------------------------------------------")
+    product_5 = create_product(correct_title, test_des_1,
+                               correct_price, correct_date, correct_email)
+    assert product_5 is None
     # Test description more than 2000 characters (2001)
     test_des_2 = ""
     j = 1
     while j < 2002:
         test_des_2 += "a"
         j += 1
-    print(create_product(correct_title, test_des_2,
-                         correct_price, correct_date, correct_email))
-    print("--------------------------------------------------------")
+    product_6 = create_product(correct_title, test_des_2,
+                               correct_price, correct_date, correct_email)
+    assert product_6 is None
     # Test when description is shorter than title
     test_des_3 = "smartwatc"
-    print(create_product(correct_title, test_des_3,
-                         correct_price, correct_date, correct_email))
-    print("--------------------------------------------------------")
+    product_7 = create_product(correct_title, test_des_3,
+                               correct_price, correct_date, correct_email)
+    assert product_7 is None
     # Test correct description length, should return nothing
-    print(create_product(correct_title, correct_description,
-                         correct_price, correct_date, correct_email))
-    print("--------------------------------------------------------")
+    product_8 = create_product(correct_title, correct_description,
+                               correct_price, correct_date, correct_email)
+    assert product_8 is None
 
     # Test price out of range (< 10)
     test_price_1 = 9
-    print(create_product(correct_title, correct_description,
-                         test_price_1, correct_date, correct_email))
-    print("--------------------------------------------------------")
+    product_10 = create_product(correct_title, correct_description,
+                                test_price_1, correct_date, correct_email)
+    assert product_10 is None
     # Test price out of range (> 10000)
     test_price_2 = 10001
-    print(create_product(correct_title, correct_description,
-                         test_price_2, correct_date, correct_email))
-    print("--------------------------------------------------------")
+    product_11 = create_product(correct_title, correct_description,
+                                test_price_2, correct_date, correct_email)
+    assert product_11 is None
 
     # Test last modified date (wrong year before 2021)
-    test_date_1 = "2020-01-03"
-    print(create_product(correct_title, correct_description,
-                         correct_price, test_date_1, correct_email))
-    print("--------------------------------------------------------")
+    test_date_1 = date(2020, 1, 3)
+    product_12 = create_product(correct_title, correct_description,
+                                correct_price, test_date_1, correct_email)
+    assert product_12 is None
     # Test last modified date (wrong year after 2025)
-    test_date_2 = "2025-01-03"
-    print(create_product(correct_title, correct_description,
-                         correct_price, test_date_2, correct_email))
-    print("--------------------------------------------------------")
+    test_date_2 = date(2025, 1, 3)
+    product_13 = create_product(correct_title, correct_description,
+                                correct_price, test_date_2, correct_email)
+    assert product_13 is None
     # Test last modified date (wrong month in year 2025)
-    test_date_3 = "2025-02-01"
-    print(create_product(correct_title, correct_description,
-                         correct_price, test_date_3, correct_email))
-    print("--------------------------------------------------------")
+    test_date_3 = date(2025, 2, 1)
+    product_14 = create_product(correct_title, correct_description,
+                                correct_price, test_date_3, correct_email)
+    assert product_14 is None
     # Test last modified date (wrong day in year 2025 and month 01)
-    test_date_4 = "2025-01-03"
-    print(create_product(correct_title, correct_description,
-                         correct_price, test_date_4, correct_email))
-    print("--------------------------------------------------------")
+    test_date_4 = date(2025, 1, 3)
+    product_15 = create_product(correct_title, correct_description,
+                                correct_price, test_date_4, correct_email)
+    assert product_15 is None
 
     # Test empty email
     test_email = ""
-    print(create_product(correct_title, correct_description,
-                         correct_price, correct_date, test_email))
-    print("--------------------------------------------------------")
-
+    product_16 = create_product(correct_title, correct_description,
+                                correct_price, correct_date, test_email)
+    assert product_16 is None
     # Check if owner exists in the database given email
     test_email_2 = "nonexistedemail@gmail.com"
-    print(create_product(correct_title, correct_description,
-                         correct_price, correct_date, test_email_2))
-    print("--------------------------------------------------------")
+    product_17 = create_product(correct_title, correct_description,
+                                correct_price, correct_date, test_email_2)
+    assert product_17 is None
 
     # Check if the product being created has the same title under the same user
     sample_username = "useruser"
@@ -143,9 +208,9 @@ def test_create_product():
     # Create a user in the database
     new_user = register(sample_username, correct_email, sample_password)
     # Create a product under the user
-    print(create_product(correct_title, correct_description,
-                         correct_price, correct_date, correct_email))
-    print("--------------------------------------------------------")
+    create_product(correct_title, correct_description,
+                   correct_price, correct_date, correct_email)
     # Attempts to create the same product title under the same user again
-    print(create_product(correct_title, correct_description,
-                         correct_price, correct_date, correct_email))
+    product_18 = create_product(correct_title, correct_description,
+                                correct_price, correct_date, correct_email)
+    assert product_18 is None
